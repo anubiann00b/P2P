52993c5be5cdc73d972e34944a5f17e33e5580bc
Tarik Onalan <phi.quantum@gmail.com>
2014-08-18 19:12:26 +0300

    Changed project compliance to JDK 1.8, Interpreter simplified

--- src/p2p/util/interpreter/Interpereter.java
+++
@@ -1,8 +0,0 @@
-package p2p.util.interpreter;
-
-import java.util.Map;
-
-public interface Interpereter {
-    
-    public Map<String, String> interperet(String s);
-}
--- src/p2p/util/interpreter/Interpreter.java
+++ src/p2p/util/interpreter/Interpreter.java
@@ -2,17 +2,19 @@ package p2p.util.interpreter;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import p2p.util.Data;
 
-public class InterpereterFirstPacket implements Interpereter {
-
-    @Override
-    public Map<String, String> interperet(String s) {
+@FunctionalInterface
+public interface Interpreter {
+    public static final Interpreter FIRST_PACKET = s -> {
         Map<String, String> map = new HashMap<String, String>();
         
         String connections = s.substring(2, 4);
         map.put(Data.NUM_CONNECTIONS, connections);
         
         return map;
-    }
+    };
+    
+    public Map<String, String> interpret(String s);
 }
