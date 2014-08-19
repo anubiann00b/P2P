764d1746bdeac8551f37c99b10481058a6587739
Shreyas <skraman1999@gmail.com>
2014-08-18 22:36:25 -0700

    Interpreter could do with line breaks.

--- src/p2p/util/Interpreter.java
+++ src/p2p/util/Interpreter.java
@@ -3,15 +3,15 @@ package p2p.util;
 import java.util.HashMap;
 import java.util.Map;
 
-import p2p.util.Data;
-
 @FunctionalInterface
 public interface Interpreter {
+    
     public static final Interpreter BASIC = s -> {
         Map<String, String> map = new HashMap<String, String>();
         map.put(Data.TYPE, s.substring(0, 2));
         return map;
     };
+    
     public static final Interpreter FIRST_PACKET = s -> {
         Map<String, String> map = new HashMap<String, String>();
         String connections = s.substring(2, 4);
